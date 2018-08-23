7eb617bf12ddae7a9b3814003fec2970b75e432a
Ep1cM1n10n <34501060+Ep1cM1n10n@users.noreply.github.com>
2018-08-23 16:35:02 -0700

    Add files via upload

--- knifefight/gamemode/init.lua
+++ knifefight/gamemode/init.lua
@@ -2,14 +2,14 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 
-function KillCounter( victim, weapon, killer )  --Sets up a new function called KillCounter
-        if killer:GetNWInt("killcounter") == 100000 then --If the killcounter variable equals 100000 then do something 
-            PrintMessage(HUD_PRINTTALK, "Player" .. killer:GetName() .. "Has Won")  --When the killcounter equals 50 it will print this "Player <playername> has won
-            timer.Simple(3, function()   --Sets up a timer for three seconds
-                game.ConsoleCommand("changelevel " ..table.Random(maps).. "\n") --When the timer finishes it excecutes this console command   
-            end)  
-        end  
-            killer:SetNWInt("killcounter", killer:GetNWInt("killcounter") + 1)  --Adds 1 everytime an NPC is killed.
-    end
+//function KillCounter( victim, weapon, killer )  --Sets up a new function called KillCounter
+        //if killer:GetNWInt("killcounter") == 100000 then --If the killcounter variable equals 100000 then do something 
+            //PrintMessage(HUD_PRINTTALK, "Player" .. killer:GetName() .. "Has Won")  --When the killcounter equals 50 it will print this "Player <playername> has won
+            //timer.Simple(3, function()   --Sets up a timer for three seconds
+                //game.ConsoleCommand("changelevel " ..table.Random(maps).. "\n") --When the timer finishes it excecutes this console command   
+            //end)  
+        //end  
+            //killer:SetNWInt("killcounter", killer:GetNWInt("killcounter") + 1)  --Adds 1 everytime an NPC is killed.
+    //end
 
-    hook.Add("PlayerDeath","KillCounter", KillCounter)
+    //hook.Add("PlayerDeath","KillCounter", KillCounter)
--- knifefight/gamemode/shared.lua
+++ knifefight/gamemode/shared.lua
@@ -7,13 +7,15 @@ models = {"models/player/kleiner.mdl", "models/player/police.mdl", "models/playe
 maps = {"gm_construct", "gm_flatgrass"}
 knifes = {"csgo_flip", "csgo_default_knife", "csgo_bayonet", "csgo_default_t", "csgo_bowie"}
 startTime = os.time()
-endTime = startTime+1000
+endTime = startTime+3000
 currentMap = game.GetMap()
+isloaddone = 0
 
 function GM:Initialize()
 end
 
 function GM:Tick()
+	//RunConsoleCommand( "give", table.Random(knifes) )
 if os.time() >= endTime then
 	game.ConsoleCommand("changelevel " ..table.Random(maps).. "\n")
 end
@@ -22,16 +24,20 @@ end
     function sql_value_stats ( ply )
 	unique_id = sql.QueryValue("SELECT unique_id FROM player_info WHERE unique_id = '"..steamID.."'")
 	kills = sql.QueryValue("SELECT kills FROM player_info WHERE unique_id = '"..steamID.."'")
+	gitkills = sql.QueryValue("SELECT kills FROM player_info WHERE unique_id = '"..steamID.."'")
 	ply:SetNWString("unique_id", unique_id)
 	ply:SetNWInt("kills", kills)
 end
  
 function saveStat ( ply )
-	kills = ply:GetNWInt("kills")
-	print( kills )
+	kills=ply:GetNWInt("killcounter")
+	print(ply:GetNWInt("killcounter"))
+	print(kills)
 	unique_id = ply:GetNWString ("SteamID")
-	sql.Query("UPDATE player_info SET kills = "..kills.." WHERE unique_id = '"..unique_id.."'")
-	ply:ChatPrint("Stats updated !")
+	print(unique_id)
+	print(ply:GetNWString ("SteamID"))
+	sql.Query("UPDATE player_info SET kills = "..kills.." WHERE unique_id = "..unique_id.."")
+	print("Stats updated !")
 end
 
 function tables_exist()
@@ -40,7 +46,7 @@ function tables_exist()
 		Msg("The table already exists!")
 	else
 		if (!sql.TableExists("player_info")) then
-			query = "CREATE TABLE player_info ( unique_id varchar(255), kills int )"
+			query = "CREATE TABLE player_info ( unique_id varchar(255), kills int, deaths int )"
 			result = sql.Query(query)
 			if (sql.TableExists("player_info")) then
 				Msg("Success ! table 1 created \n")
@@ -88,13 +94,49 @@ function PlayerInitialSpawn( ply )
 		timer.Create("SaveStat", 10, 0, function() saveStat( ply ) end)
 		player_exists( ply ) 
 	end)
+ply:SetModel( table.Random(models) )
+
 end
- 
-function GM:PlayerSpawn( ply )
-	RunConsoleCommand( "give", table.Random(knifes) )
-	ply:SetModel( table.Random(models) )
+
+function GM:PlayerLoadout( ply )
+	ply:Give( "csgo_bowie" )
+	ply:Give( "csgo_default_knife" )
+	ply:Give( "csgo_flip" )
+	ply:Give( "csgo_default_t" )
+	ply:Give( "csgo_bayonet" )
+
+
+	-- Prevent default Loadout.
+	return true
 end
 
 hook.Add( "PlayerInitialSpawn", "PlayerInitialSpawn", PlayerInitialSpawn )
 hook.Add( "Initialize", "Initialize", Initialize )
-hook.Add( "PlayerSpawn", "PlayerSpawn" )
+hook.Add( "PlayerLoadout", "PlayerLoadout" )
+
+function KillCounter( victim, weapon, killer )  --Sets up a new function called KillCounter
+	            	if result~=nul then
+            		if isloaddone==0 then
+            killer:SetNWInt("killcounter", killer:GetNWInt("killcounter") + gitkills)
+            print(isloaddone)
+            isloaddone = 1
+            print(isloaddone)
+        else
+        	//not used
+        end
+    end
+        if killer:GetNWInt("killcounter") == 100000 then --If the killcounter variable equals 100000 then do something 
+            PrintMessage(HUD_PRINTTALK, "Player" .. killer:GetName() .. "Has Won")  --When the killcounter equals 50 it will print this "Player <playername> has won
+            timer.Simple(3, function()   --Sets up a timer for three seconds
+                game.ConsoleCommand("changelevel " ..table.Random(maps).. "\n") --When the timer finishes it excecutes this console command   
+            end)  
+        end
+            if killer~=victim then
+            killer:SetNWInt("killcounter", killer:GetNWInt("killcounter") + 1)
+
+        
+        end
+
+    end
+
+hook.Add("PlayerDeath","KillCounter", KillCounter)
