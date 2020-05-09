1270469e9591853afc737e5feec7d2f3570b2f75
PoltixeTheDerg <34501060+PoltixeTheDerg@users.noreply.github.com>
2020-05-08 19:50:29 -0700

    Refactor code
    Refactor a lot of code along with adding a .gitignore

--- gamemodes/knifefight/gamemode/loadout.lua
+++ gamemodes/knifefight/gamemode/loadout.lua
@@ -1,32 +1,35 @@
-//This function runs every tick
+--This function runs every tick
 function GM:Tick()
-	grenades = GetConVar("kf_grenades"):GetInt() //This stuff sets variables every tick
+	grenades = GetConVar("kf_grenades"):GetInt() --This stuff sets variables every tick
 	phys = GetConVar("kf_phys"):GetInt()
 end
-//This function gives the player the set loadout
-function GM:PlayerLoadout( ply )
-	ply:Give( "csgo_bowie" )
-	ply:Give( "csgo_default_knife" )
-	ply:Give( "csgo_flip" )
-	ply:Give( "csgo_default_t" )
-	ply:Give( "csgo_bayonet" )
-if grenades == 1 then
-	ply:Give( "weapon_frag" )
+
+--This function gives the player the set loadout
+function GM:PlayerLoadout(ply)
+	ply:Give("csgo_bowie")
+	ply:Give("csgo_default_knife")
+	ply:Give("csgo_flip")
+	ply:Give("csgo_default_t")
+	ply:Give("csgo_bayonet")
+	if grenades == 1 then
+		ply:Give("weapon_frag")
 		if phys == 1 then
-		ply:Give( "weapon_physcannon")
+			ply:Give("weapon_physcannon")
+		end
 	end
-end
 	-- Prevent default Loadout.
 	return true
 end
 
-
-//These are just the hooks used by my code
-hook.Add( "PlayerLoadout", "PlayerLoadout" )
-hook.Add( "PlayerSpawn", "playerSetSpeedtest", playerSetSpeed )
-//This sets the players speed
+--These are just the hooks used by my code
+hook.Add("PlayerLoadout", "PlayerLoadout")
+hook.Add("PlayerSpawn", "playerSetSpeedtest", playerSetSpeed)
+--This sets the players speed
 function playerSetSpeed(ply)
-timer.Simple(2, function()
-    GAMEMODE:SetPlayerSpeed(ply, 130, 250)
-end)
+	timer.Simple(
+		2,
+		function()
+			GAMEMODE:SetPlayerSpeed(ply, 130, 250)
+		end
+	)
 end
--- gamemodes/knifefight/gamemode/shared.lua
+++ gamemodes/knifefight/gamemode/shared.lua
@@ -2,26 +2,39 @@ GM.Name = "Knife Fight"
 GM.Author = "Ep1c_M1n10n"
 GM.Email = "ep1cm1n10n123@gmail.com"
 GM.Website = "ep1cm1n10n.ddns.net"
-models = {"models/player/kleiner.mdl", "models/player/police.mdl", "models/player/police_fem.mdl", "models/player/combine_soldier.mdl", "models/player/combine_super_soldier.mdl", "models/player/combine_soldier_prisonguard.mdl", "models/player/Group01/male_07.mdl"}
-maps = {"gm_construct", "gm_flatgrass", "kf_spheres", "kf_quakena", "kf_arena"}
-knifes = {"csgo_flip", "csgo_default_knife", "csgo_bayonet", "csgo_default_t", "csgo_bowie"}
-startTime = os.time()
-currentMap = game.GetMap()
+local models = {
+	"models/player/kleiner.mdl",
+	"models/player/police.mdl",
+	"models/player/police_fem.mdl",
+	"models/player/combine_soldier.mdl",
+	"models/player/combine_super_soldier.mdl",
+	"models/player/combine_soldier_prisonguard.mdl",
+	"models/player/Group01/male_07.mdl"
+}
+local maps = {"gm_construct", "gm_flatgrass", "kf_spheres", "kf_quakena", "kf_arena"}
+local knifes = {"csgo_flip", "csgo_default_knife", "csgo_bayonet", "csgo_default_t", "csgo_bowie"}
+local startTime = os.time()
+local currentMap = game.GetMap()
+
 function GM:Initialize()
 end
+
 function GM:Tick()
-timelimit = GetConVar("kf_timelimit"):GetInt()*60
-endTime = startTime+timelimit
-timeleft = os.time()-endTime
-if os.time() >= endTime then
-	game.ConsoleCommand("changelevel " ..table.Random(maps).. "\n")
+	local timelimit = GetConVar("kf_timelimit"):GetInt() * 60
+	local endTime = startTime + timelimit
+	local timeleft = os.time() - endTime
+
+	if os.time() >= endTime then
+		game.ConsoleCommand("changelevel " .. table.Random(maps) .. "\n")
+	end
 end
+
+function PlayerInitialSpawn(ply)
+	ply:SetModel(table.Random(models))
 end
-function PlayerInitialSpawn( ply )
-ply:SetModel( table.Random(models) ) 
-end
-hook.Add( "PlayerInitialSpawn", "PlayerInitialSpawn", PlayerInitialSpawn )
-hook.Add( "Initialize", "Initialize", Initialize )
+
+hook.Add("PlayerInitialSpawn", "PlayerInitialSpawn", PlayerInitialSpawn)
+hook.Add("Initialize", "Initialize", Initialize)
 
 function GM:PlayerSwitchFlashlight(ply, SwitchOn)
 	return true
